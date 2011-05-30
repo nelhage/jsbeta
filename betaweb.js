7b025dd48966a228cff5f8ee2845dd87277642cc
Nelson Elhage <nelhage@nelhage.com>
2011-05-30 14:53:15 -0400

    Add a play/pause button, style controls a little.

--- betaweb.js
+++ betaweb.js
@@ -1,9 +1,18 @@
 var betaTerm;
+var betaDiv;
 
 function initBeta() {
     betaTerm = new Terminal( {handler: termHandler, initHandler: initTerm} );
     betaTerm.charMode = true;
     betaTerm.open();
+
+    betaDiv = document.getElementById('termDiv');
+    betaDiv.onmousedown = mouseHandler;
+    resetBeta();
+}
+
+function resetBeta() {
+    betaTerm.clear();
 
     MMU.load(lab8_rom);
     CPU.reset({timer: true,
@@ -16,20 +25,32 @@ function initBeta() {
                halt: function() {
                    term.type("--- Program terminated ----");
                }});
-    CPU.run();
+    playPauseBeta();
+}
 
-    var div = document.getElementById('termDiv');
-    div.onmousedown = mouseHandler;
+function playPauseBeta() {
+    var button = document.getElementById('playplausebutton');
+    if (!button)
+        return;
+    if(CPU.running()) {
+        CPU.pause();
+        button.value="run";
+    } else {
+        CPU.run();
+        button.value="pause";
+    }
 }
 
 function initTerm() {
     this.lock = false;
 }
 function termHandler() {
+    if (!CPU.running()) return;
     var ch = this.inputChar;
     if (ch == 13) ch = 10;
     CPU.press_key(ch);
 }
 function mouseHandler(evt) {
+    if (!CPU.running()) return;
     CPU.click(evt.x, evt.y);
 }
