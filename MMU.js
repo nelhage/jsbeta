b59ba9add47d25f5e21d673343bf9c1e2ad5d898
Nelson Elhage <nelhage@nelhage.com>
2011-05-31 22:14:12 -0400

    Fix memory access checks for accesses one past the end of the ROM.

--- MMU.js
+++ MMU.js
@@ -5,14 +5,14 @@ var MMU = {
     },
     read: function(addr) {
         var ea = (addr & ~CPU.PC_SUPERVISOR) >> 2;
-        if (ea > MMU.memory.length) {
+        if (ea >= MMU.memory.length) {
             throw new Error("Invalid read: " + (addr & ~CPU.PC_SUPERVISOR));
         }
         return MMU.memory[ea];
     },
     write: function(addr, data) {
         var ea = (addr & ~CPU.PC_SUPERVISOR) >> 2;
-        if (ea > MMU.memory.length) {
+        if (ea >= MMU.memory.length) {
             throw new Error("Invalid read: " + (addr & ~CPU.PC_SUPERVISOR));
         }
         MMU.memory[ea] = data;
